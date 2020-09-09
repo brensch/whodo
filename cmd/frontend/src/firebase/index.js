b5709d688954588d986346d81c7d0c63da2a9afa
brendo <brendolini@protonmail.com>
2020-09-10 02:29:29 +1000

    added secrets securely

--- cmd/frontend/src/firebase/index.js
+++ cmd/frontend/src/firebase/index.js
@@ -1,8 +1,7 @@
 import firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
-
-const firebaseConfig = {};
+import firebaseConfig from "./firestore_secrets.json";
 
 firebase.initializeApp(firebaseConfig);
 
