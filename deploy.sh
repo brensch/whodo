4922f7c5e5278882242f0ec86144ca3f673ddc24
brendo <brendolini@protonmail.com>
2020-10-31 01:49:34 +1100

    removed superfluous script

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-gcloud functions deploy HelloCool \
-  --runtime go113 \
-  --trigger-event providers/cloud.firestore/eventTypes/document.write \
-  --trigger-resource "projects/whodo-app/databases/(default)/documents/stories2/{story}" \
-  --source cmd/stories/
